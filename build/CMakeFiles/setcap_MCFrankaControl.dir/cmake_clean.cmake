4f706ade629b55fe14a062e16e5bf957b0c95f58
mohamed djeha <mohamed.djeha@lirmm.fr>
2021-01-04 11:38:05 +0100

    remove build

--- build/CMakeFiles/setcap_MCFrankaControl.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/setcap_MCFrankaControl"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/setcap_MCFrankaControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
